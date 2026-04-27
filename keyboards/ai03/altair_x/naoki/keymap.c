1162fc79899f22fead2efb220968d7b3d8d9ae36
naoking158 <29372455+naoking158@users.noreply.github.com>
2026-04-27 20:53:10 +0900

    update

--- keyboards/ai03/altair_x/naoki/keymap.c
+++ keyboards/ai03/altair_x/naoki/keymap.c
@@ -29,30 +29,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
                  LGUI_T(KC_TAB), KC_Q,  KC_W,  KC_E,   KC_R,        KC_T,  LCTL(KC_GRV),    KC_DEL,   KC_Y,    KC_U,      KC_I,    KC_O,     KC_P,          KC_BSPC,
-                 KC_LCTL,        KC_A,  KC_S,  KC_D,   KC_F,  LT(3,KC_G),       KC_HOME,    KC_END,   KC_H,    KC_J,      KC_K,    KC_L,  KC_SCLN,  RALT_T(KC_QUOT),
+                 KC_LCTL,        KC_A,  KC_S,  KC_D,   KC_F,  LT(3,KC_G),       KC_HOME,    KC_END,   KC_H,    KC_J,      KC_K,    KC_L,  KC_SCLN,  LALT_T(KC_QUOT),
                  KC_LSFT,        KC_Z,  KC_X,  KC_C,   KC_V,        KC_B,     SCRN_SHOT,  SCRN_REC,   KC_N,    KC_M,   KC_COMM,  KC_DOT,  LT(3,KC_SLSH),    KC_ENT,
-                 LALT_T(KC_ESC),   KC_LGUI,    MO(2),   LT(1,KC_SPC),                LT(1,KC_SPC),    MO(2),   KC_RGUI,   KC_RALT
+                 LALT_T(KC_ESC),   KC_LGUI,    MO(2),   LT(1,KC_SPC),                LT(1,KC_SPC),    MO(2),   KC_LGUI,   KC_LALT
                  ),
 
     [1] = LAYOUT(
                  LGUI_T(KC_TAB),  KC_EXLM,    KC_AT,    KC_HASH,    KC_DLR,    KC_PERC,    XXXXXXX, XXXXXXX, KC_CIRC,    KC_AMPR,      KC_ASTR,    KC_LPRN,    KC_RPRN,      KC_BSPC,
-                 KC_LCTL,         _______,    _______,    KC_UP,    _______,    KC_RGUI,    XXXXXXX, XXXXXXX, KC_LEFT,    KC_DOWN,      KC_UP,    KC_RIGHT,    KC_LBRC, RALT_T(KC_QUOT),
-                 KC_LSFT,         _______,    KC_LEFT,    KC_DOWN,    KC_RIGHT,  RGUI(KC_RALT),  XXXXXXX, XXXXXXX, _______,    _______,   KC_COMM,  KC_DOT,    KC_SLSH,          KC_ENT,
-                 LALT_T(KC_ESC), KC_LGUI, MO(2),  LT(1,KC_SPC),                LT(1,KC_SPC), MO(2),   KC_RGUI, KC_RALT
+                 KC_LCTL,         _______,    _______,    KC_UP,    _______,    KC_LGUI,    XXXXXXX, XXXXXXX, KC_LEFT,    KC_DOWN,      KC_UP,    KC_RIGHT,    KC_LBRC, LALT_T(KC_QUOT),
+                 KC_LSFT,         _______,    KC_LEFT,    KC_DOWN,    KC_RIGHT,  LGUI(KC_LALT),  XXXXXXX, XXXXXXX, _______,    _______,   KC_COMM,  KC_DOT,    KC_SLSH,          KC_ENT,
+                 LALT_T(KC_ESC), KC_LGUI, MO(2),  LT(1,KC_SPC),                LT(1,KC_SPC), MO(2),   KC_LGUI, KC_LALT
                  ),
 
     [2] = LAYOUT(
                  LGUI_T(KC_TAB),  KC_1,    KC_2,    KC_3,    KC_4,  KC_5,    XXXXXXX, XXXXXXX, KC_6,    KC_7,      KC_8,    KC_9,    KC_0,            KC_PLUS,
-                 KC_LCTL,         KC_GRV,    KC_LBRC,    KC_LCBR,    KC_LPRN,    KC_MINUS,    XXXXXXX, XXXXXXX, KC_EQL,    KC_RPRN,      KC_RCBR,    KC_RBRC,    KC_BACKSLASH, RALT_T(KC_QUOT),
+                 KC_LCTL,         KC_GRV,    KC_LBRC,    KC_LCBR,    KC_LPRN,    KC_MINUS,    XXXXXXX, XXXXXXX, KC_EQL,    KC_RPRN,      KC_RCBR,    KC_RBRC,    KC_BACKSLASH, LALT_T(KC_QUOT),
                  KC_LSFT,         KC_TILDE,    _______,    _______,    _______,  KC_UNDS,    XXXXXXX, XXXXXXX, KC_PLUS,    _______,   KC_COMM,  KC_DOT,    KC_PIPE,          KC_ENT,
-                 LALT_T(KC_ESC), KC_LGUI, MO(2),  LT(1,KC_SPC),                LT(1,KC_SPC), MO(2),   KC_RGUI, KC_RALT
+                 LALT_T(KC_ESC), KC_LGUI, MO(2),  LT(1,KC_SPC),                LT(1,KC_SPC), MO(2),   KC_LGUI, KC_LALT
                  ),
 
     [3] = LAYOUT(
                  LGUI_T(KC_TAB),  KC_F1,    KC_F2,    KC_F3,    KC_F4,  KC_F5,    QK_BOOT, QK_BOOT, KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,
                  KC_LCTL,         KC_BRIU,    KC_BRID,    KC_MCTL,    KC_MCTL,    _______,    XXXXXXX, XXXXXXX,  _______,    KC_MPRV,    KC_MPLY,   KC_MNXT,    KC_MUTE,  KC_F12,
                  KC_LSFT,        _______,    KC_LEFT,    KC_DOWN,    KC_RIGHT,    _______,    XXXXXXX, XXXXXXX, KC_N,    KC_M,   KC_COMM,  KC_DOT,    KC_SLSH,          KC_ENT,
-                 LALT_T(KC_ESC), KC_LGUI, MO(2),  LT(1,KC_SPC),                LT(1,KC_SPC), MO(2),   KC_RGUI, KC_RALT
+                 LALT_T(KC_ESC), KC_LGUI, MO(2),  LT(1,KC_SPC),                LT(1,KC_SPC), MO(2),   KC_LGUI, KC_LALT
                  ),
 };
 
